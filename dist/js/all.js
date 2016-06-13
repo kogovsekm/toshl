8f8561cf72cada0aaf9762b32d3647255a0d0a15
kogovsekm <kogovsek.matija@gmail.com>
2016-06-13 23:03:14 +0200

    added SecondController to monitor dates - not yet finished

--- dist/js/all.js
+++ dist/js/all.js
@@ -193,6 +193,22 @@ angular.module('app').controller('MainController', function($scope, $q, $http){
 
 });
 
+angular.module('app').controller('dateController', function($scope, $q, $http){
+
+//add date filter with /?start_at=&end_at= method - not yet implemented
+
+var url_date = 'http://toshl-killer.herokuapp.com/api/v1/balance_changes/?start_at=?&end_at=?';
+
+var date = [];
+
+$http.get(url_date).then(
+      function(success) {
+          $scope.date = success.data;
+
+      });
+
+});
+
 angular.module("app").controller("overviewController", function($scope) {
 
     
--- src/components/main/dateController.js
+++ src/components/main/dateController.js
@@ -1,13 +1,15 @@
 angular.module('app').controller('dateController', function($scope, $q, $http){
 
-var url_date = 'http://toshl-killer.herokuapp.com/api/v1/balance_changes';
+//add date filter with /?start_at=&end_at= method - not yet implemented
+
+var url_date = 'http://toshl-killer.herokuapp.com/api/v1/balance_changes/?start_at=?&end_at=?';
 
 var date = [];
 
 $http.get(url_date).then(
       function(success) {
           $scope.date = success.data;
-      
+
       });
 
 });
